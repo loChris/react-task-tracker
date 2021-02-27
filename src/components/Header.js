2e63002d25aa640a30b2bffca96c31f12ec324ff
Chris <44450518+loChris@users.noreply.github.com>
2021-02-27 15:14:44 -0400

    set up events and props for the button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from './Button';
 
 const Header = ({ title }) => {
+	const onClick = (e) => {
+		console.log(e);
+	};
+
 	return (
 		<header className="header">
 			<h1>{title}</h1>
-			<button className="btn">Add</button>
+			<Button color="Green" text="Add" onClick={onClick} />
 		</header>
 	);
 };
