06bb136244af3f87ee35cc40e4a08d4f68adf33c
loChris <44450518+loChris@users.noreply.github.com>
2021-03-01 13:03:22 -0400

    Added json-server to use as a mock db

--- src/App.js
+++ src/App.js
@@ -6,32 +6,7 @@ import { useState } from 'react';
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false);
 
-	const [tasks, setTasks] = useState([
-		{
-			id: 1,
-			task: 'Doctors appointment',
-			date: 'Feb 5th at 2pm',
-			reminder: true,
-		},
-		{
-			id: 2,
-			task: 'Meeting at school',
-			date: 'Feb 1st at 11am',
-			reminder: true,
-		},
-		{
-			id: 3,
-			task: 'Food shopping',
-			date: 'Feb 1st at 2pm',
-			reminder: false,
-		},
-		{
-			id: 4,
-			task: 'Task due',
-			date: 'Feb 10th at 11pm',
-			reminder: true,
-		},
-	]);
+	const [tasks, setTasks] = useState([]);
 
 	const addTask = (task) => {
 		const id = Math.floor(Math.random() * 10000) + 1;
@@ -55,7 +30,10 @@ function App() {
 
 	return (
 		<div className="App">
-			<Header onAddClick={() => setShowAddTask(!showAddTask)} />
+			<Header
+				onAddClick={() => setShowAddTask(!showAddTask)}
+				showAddTask={showAddTask}
+			/>
 
 			{showAddTask && <AddTask onAddClick={addTask} />}
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({ title, onAddClick }) => {
+const Header = ({ title, onAddClick, showAddTask }) => {
 	return (
 		<header className="header">
 			<h1>{title}</h1>
-			<Button color="Green" text="Add" onClick={onAddClick} />
+			<Button
+				color={showAddTask ? 'red' : 'green'}
+				text={showAddTask ? 'Close' : 'Add'}
+				onClick={onAddClick}
+			/>
 		</header>
 	);
 };
