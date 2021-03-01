a21077454274e9dd0daf774abdd1e823de536f1f
loChris <44450518+loChris@users.noreply.github.com>
2021-03-01 13:27:34 -0400

    Added/New tasks are now posted to the json-server

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
 import Header from './components/Header';
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 	const [showAddTask, setShowAddTask] = useState(false);
 
 	const [tasks, setTasks] = useState([]);
 
-	const addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
-		const newTask = { id, ...task };
-		console.log(task);
+	useEffect(() => {
+		const getTasks = async () => {
+			const tasksFromServer = await fetchTasks();
+			setTasks(tasksFromServer);
+		};
+		getTasks();
+	}, []);
 
-		setTasks([...tasks, newTask]);
+	// fetch tasks from db.json
+	const fetchTasks = async () => {
+		const res = await fetch('http://localhost:5000/tasks');
+		const data = await res.json();
+		return data;
 	};
 
-	const deleteTask = (id) => {
+	const addTask = async (task) => {
+		const res = await fetch('http://localhost:5000/tasks', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json',
+			},
+			body: JSON.stringify(task),
+		});
+
+		const data = await res.json();
+		setTasks([...tasks, data]);
+
+		// const newTask = { id, ...task };
+		// console.log(task);
+
+		// setTasks([...tasks, newTask]);
+	};
+
+	const deleteTask = async (id) => {
+		await fetch(`http://localhost:5000/tasks/${id}`, {
+			method: 'DELETE',
+		});
+
 		setTasks(tasks.filter((task) => task.id !== id));
 	};
 
@@ -35,7 +64,7 @@ function App() {
 				showAddTask={showAddTask}
 			/>
 
-			{showAddTask && <AddTask onAddClick={addTask} />}
+			{showAddTask && <AddTask onAddTask={addTask} />}
 
 			{tasks.length > 0 ? (
 				<Tasks
